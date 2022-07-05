c12e01ef74861862e53b8b29b4d383accdf1e75e
= <=>
2022-07-05 15:15:23 +0700

    dear hieunm

---
+++ Whowroteit/app/src/main/java/com/example/whowroteit/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.whowroteit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
+    private BookListFragment bookListFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBookInput = findViewById(R.id.bookInput);
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.authorText);
+
+        bookListFragment = new BookListFragment(this, mTitleText, mAuthorText);
+
+        if (getSupportLoaderManager().getLoader(0) != null) {
+            getSupportLoaderManager().initLoader(0, null, bookListFragment);
+        }
+    }
+
+    /**
+     * onClick handler for the "Search Books" button.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void searchBooks(View view) {
+        // Get the search string from the input field.
+        String queryString = mBookInput.getText().toString();
+
+        // Hide the keyboard when the button is pushed.
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputManager != null) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        // Check the status of the network connection.
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = null;
+        if (connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+
+        // If the network is available, connected, and the search field
+        // is not empty, start a BookLoader AsyncTask.
+        if (networkInfo != null && networkInfo.isConnected()
+                && queryString.length() != 0) {
+
+            Bundle queryBundle = new Bundle();
+            queryBundle.putString("queryString", queryString);
+            getSupportLoaderManager().restartLoader(0, queryBundle, bookListFragment);
+
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        }
+        // Otherwise update the TextView to tell the user there is no
+        // connection, or no search term.
+        else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
+    }
+
+}
